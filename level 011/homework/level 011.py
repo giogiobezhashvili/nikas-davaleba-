704d77c313420cae30686394b4d9624d541a3b50
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-09-15 21:59:47 +0400

    second commit

---
+++ level 011/homework/level 011.py
@@ -0,0 +1,37 @@
+# 2
+# for ციკლი გამოიყენება მაშინ როვა რაღაცის გამეორება გვინდა/
+# მაგ. №1
+for i in range (5):
+    print (i)
+
+# მაგ. №2
+
+for i in range(1,15):
+    print(i)
+
+# მაგ. №3 
+
+for i in range (0,49,4):
+    print(i)
+
+
+# 3
+
+for a in range(1,101):
+    print(a)
+
+# 4
+for b in range(1,100,5):
+    print(b)
+
+# 5
+
+for c in range (5,87,2):
+    print(c+1)
+
+#6
+
+name = input("შეიყვანთ თქვენი სახელი: ")
+
+for i in range(15):
+    print(name)
